dc2dd16e4833292ee7552fa6231ff013c92642b1
ianramdhan <ianramdhan30@gmail.com>
2022-06-18 13:13:30 +0700

    getListToko clear

--- app/src/main/java/com/project/mjsmanagementapp/data/ApiInterface.kt
+++ app/src/main/java/com/project/mjsmanagementapp/data/ApiInterface.kt
@@ -1,15 +1,15 @@
 package com.project.mjsmanagementapp.data
 
 import com.project.mjsmanagementapp.model.Toko.getListToko.ResponseListToko
+import com.project.mjsmanagementapp.model.Toko.getListToko.ResponseListTokoItem
 import retrofit2.Call
 import retrofit2.http.GET
 
 interface ApiInterface {
 
     //GET LIST TOKO
-    @GET("toko/getListToko")
-    fun getListToko() : Call<ResponseListToko>
-    fun getToko() : Call<List<ResponseListToko>>
+    @GET("toko/getListToko.php")
+    fun getToko() : Call<List<ResponseListTokoItem>>
 
 
 }
--- app/src/main/java/com/project/mjsmanagementapp/ui/Toko/listToko/ListTokoActivity.kt
+++ app/src/main/java/com/project/mjsmanagementapp/ui/Toko/listToko/ListTokoActivity.kt
@@ -1,13 +1,16 @@
 package com.project.mjsmanagementapp.ui.Toko.listToko
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.project.mjsmanagementapp.MainActivity
 import com.project.mjsmanagementapp.R
 import com.project.mjsmanagementapp.adapter.Toko.ListToko.ListTokoAdapter
 import com.project.mjsmanagementapp.model.Toko.getListToko.ResponseListTokoItem
 import com.project.mjsmanagementapp.ui.Toko.detailToko.DetailTokoActivity
 import kotlinx.android.synthetic.main.tokolist_activity.*
+import org.jetbrains.anko.sdk27.coroutines.onClick
 import org.jetbrains.anko.startActivity
 
 
@@ -18,12 +21,16 @@ class ListTokoActivity : AppCompatActivity(), ListTokoActivityContract {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.tokolist_activity)
         presenter = ListTokoActivityPresenter(this)
-        presenter.getListToko()
+        presenter.getTokoList()
 
-        val linearLayoutManager: LinearLayoutManager = LinearLayoutManager(this)
+        val linearLayoutManager:LinearLayoutManager = LinearLayoutManager(this)
         linearLayoutManager.orientation = LinearLayoutManager.VERTICAL
         rvListToko.layoutManager = linearLayoutManager
 
+        btnimgBack.onClick {
+            startActivity<MainActivity>()
+            finish()
+        }
 
 
     }
@@ -38,6 +45,7 @@ class ListTokoActivity : AppCompatActivity(), ListTokoActivityContract {
     }
 
     override fun onErrorGetList(msg: String?) {
+        Toast.makeText(applicationContext, "Error Ya", Toast.LENGTH_SHORT).show()
 
     }
 
