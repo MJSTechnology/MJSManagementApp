eada96734cc58b2ad5b419a001ad5c6d838e15a6
rizieq <rizieqgifari13012003@gmail.com>
2022-07-09 17:20:12 +0700

    # push app v2

--- app/src/main/java/com/project/mjsmanagementapp/data/ApiClient.kt
+++ app/src/main/java/com/project/mjsmanagementapp/data/ApiClient.kt
@@ -6,7 +6,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object ApiClient {
-    val BASE_URL: String = "http://mutijayasejahtera.com/mjs-api-testing/"
+    val BASE_URL: String = "http://mutijayasejahtera.com/mjs-api/"
 
     fun getInterceptor() : OkHttpClient {
         val logging = HttpLoggingInterceptor()
