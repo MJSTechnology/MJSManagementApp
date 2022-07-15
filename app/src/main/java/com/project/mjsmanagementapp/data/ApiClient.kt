099c4dcbf95d4bbc2faf7ce6b7bcb277b08da288
ianramdhan <ianramdhan30@gmail.com>
2022-07-15 14:13:18 +0700

    Ian 15 July #1

--- app/src/main/java/com/project/mjsmanagementapp/data/ApiClient.kt
+++ app/src/main/java/com/project/mjsmanagementapp/data/ApiClient.kt
@@ -6,7 +6,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object ApiClient {
-    val BASE_URL: String = "http://mutijayasejahtera.com/mjs-api/"
+    val BASE_URL: String = "http://mutijayasejahtera.com/mjs-api-testing/"
 
     fun getInterceptor() : OkHttpClient {
         val logging = HttpLoggingInterceptor()
